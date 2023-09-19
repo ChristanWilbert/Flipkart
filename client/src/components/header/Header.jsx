0cdbfe1cf3e6c36f11c529bdd18bca18209b015b
Christan Wilbert <cwchristan@gmail.com>
2023-09-19 17:08:14 +0530

    Navbar UI

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,48 @@
 import styled from "@emotion/styled";
-import { AppBar, Box, Toolbar } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import Search from "./Search";
+import CustomButtons from "./CustomButtons";
+
 const StyledHeader = styled(AppBar)`
   background: #2874f0;
   height: 55px;
 `;
-const logoUrl =
-  "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+
+const Component = styled(Box)`
+  margin-left: 12%;
+  line-height: 0;
+`;
+
+const SubHeading = styled(Typography)`
+  font-size: 10px;
+  font-style: italic;
+`;
+
+const PlusImage = styled("img")({ width: 10, marginLeft: 4, height: 10 });
 const Header = () => {
+  const logoUrl =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+
+  const subURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
   return (
     <StyledHeader>
-      <Toolbar>
+      <Toolbar style={{ minHeight: 55 }}>
+        <Component>
+          <img src={logoUrl} alt="logo" width={80} />
+          <Box display={"flex"}>
+            <SubHeading>
+              Explore{" "}
+              <Box component="span" style={{ color: "#FFE500" }}>
+                Plus
+              </Box>
+              <PlusImage src={subURL} alt="sublogo" />
+            </SubHeading>
+          </Box>
+        </Component>
+        <Search />
         <Box>
-          <img src={logoUrl} alt="logo" style={{ width: 80 }} />
+          <CustomButtons />
         </Box>
       </Toolbar>
     </StyledHeader>
